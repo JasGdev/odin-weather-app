32ad2af2d1753e79e6df885bd952a6159becec3a
Jason <jason13.deng@gmail.com>
2026-02-07 22:24:46 +0900

    Implement unit selection, and day/night background

--- src/index.js
+++ src/index.js
@@ -29,8 +29,14 @@ async function getUsableData(url) {
 }
 
 const searchForm = document.querySelector("form");
-const searchInput = document.querySelector("input");
+const searchInput = document.querySelector(".search");
 const msgDisplay = document.querySelector(".msg");
+const fahrenheitRadio = document.getElementById('Fahrenheit')
+const container = document.querySelector('.container')
+const labels = document.querySelectorAll('label')
+
+
+
 
 searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -38,18 +44,77 @@ searchForm.addEventListener("submit", (e) => {
   const accessURL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city.toLowerCase()}?key=B7866RFQXY9AH8LCVSL22JW3T`;
 
   getUsableData(accessURL).then((data) => {
-    console.log(data)
-    // displayWeather(data.conditions)
-    const location =
-      data.location.charAt(0).toUpperCase() + data.location.slice(1);
+    //   06:00–17:59 → day
+// 18:00–05:59 → night
+    const time = data.currentTime
+    let dayNight = ''
+
+
+
+
+    if (time > '06:00' && time < '18:00'){
+      dayNight = 'day' 
+      if (container.classList.contains('night')){
+        container.classList.remove('night')
+        labels.forEach((label) => {
+          label.classList.remove('night')
+        })
+      }
+      container.classList.add('day')
+      labels.forEach((label) => {
+          label.classList.add('day')
+        })
+
+      
+    } else  {
+      dayNight = 'night' 
+      if (container.classList.contains('day')){
+        container.classList.remove('day')
+        labels.forEach((label) => {
+          label.classList.remove('day')
+        })
+      }
+      container.classList.add('night')
+      labels.forEach((label) => {
+          label.classList.add('night')
+        })
+      
+    }
+    console.log(dayNight)
+
+    
+   
+
+
+
+
+
+
+    displayWeather(`${data.conditions} at ${dayNight}`)
+    
     const conditions = data.conditions.toLowerCase();
     const currentTime = data.currentTime;
     const weekConditions = data.weekConditions.toLowerCase();
-    const measurement = "Celsius";
-    const temp = data.temp + " " + measurement;
-    const feelsLike = data.feelsLike + " " + measurement;
+    let temp = data.temp;
+    let feelsLike = data.feelsLike;
 
-    msgDisplay.textContent = `${location} at ${currentTime} is currently ${conditions}, it is currently ${temp} but it feels like ${feelsLike}. The week forecast is ${weekConditions}`;
+    if (fahrenheitRadio.checked){
+      const measurement = 'fahrenheit'
+      temp = `${temp} ${measurement}`
+      feelsLike = `${feelsLike} ${measurement}`
+    } else {
+      const measurement = 'celsius'
+      temp = `${fahrenheitToCelsius(temp)} ${measurement}`
+      feelsLike = `${fahrenheitToCelsius(feelsLike)} ${measurement}`
+
+    }
+
+    const location =
+      city.charAt(0).toUpperCase() + city.slice(1);
+    
+
+    msgDisplay.classList.add('visible')
+    msgDisplay.textContent = `${location} is currently ${currentTime} and is ${conditions}, it is currently ${temp} but it feels like ${feelsLike}. The week forecast is ${weekConditions}`;
     console.log(data);
   });
 });
@@ -57,9 +122,16 @@ searchForm.addEventListener("submit", (e) => {
 const img = document.querySelector("img");
 
 async function displayWeather(conditions) {
+
+
+
   const response = await fetch(
-    `https://api.giphy.com/v1/gifs/translate?api_key=twFGArayW07V8dN1iso8otCHe1H4GhKa&s=weather${conditions}`,
+    `https://api.giphy.com/v1/gifs/translate?api_key=twFGArayW07V8dN1iso8otCHe1H4GhKa&s=${conditions} weather`,
   );
   const weatherData = await response.json();
   img.src = weatherData.data.images.original.url;
+}
+
+function fahrenheitToCelsius(temp){
+  return ((temp-32)*5/9).toFixed(2)
 }
